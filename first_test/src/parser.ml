e47166ef791f33e21c479b47432c1993278c52ad
Charlotte Thomas <contact@nwa2coco.fr>
2022-01-25 16:53:28 +0100

    String parsing replaced by a much more elegant recursive function

--- first_test/src/parser.ml
+++ first_test/src/parser.ml
@@ -5,11 +5,13 @@ module Parser = struct
   type parameters = CallExpression of string | Argument of arguments;;
   type 'a ast = Nil | Node of 'a * 'a list ast;;
 
-  let parse_string index_start token_lst = 
-    let s = ref "" and n = Array.length token_lst and i_a = ref (index_start+1) in 
-    while !i_a < (n-1) && (match token_lst.(!i_a) with Token.QUOTE -> false | _ -> true) do
-      s := !s ^ (Token.token_to_litteral_string (token_lst.(!i_a)));
-      i_a := !i_a + 1
-    done;
-    (!s,!i_a)
+  let parse_string_rec lst = 
+    let rec parse acc lst = 
+      match lst with 
+        | [] -> ( acc,[])
+        | Token.QUOTE::q -> (acc,q)
+        | token::q -> parse (acc ^ (Token.token_to_litteral_string token)) q
+    in parse "" lst;;
+    
+
 end
--- first_test/test/test.ml
+++ first_test/test/test.ml
@@ -3,6 +3,10 @@ include Lexer
 include Token
 include Parser
 
+let str = "CHOUQUETTE PARISBREST lol 3 pdpkdqopdq 45 () () () ) CHOUQUETTE PARISBREST CLAFOUTIS"
+
+let () = print_string "Affichage de la ligne de code"; print_newline (); print_string str; print_newline (); print_newline ()
+
 let print_token_list list =
   let rec str acc list = 
     match list with
@@ -12,13 +16,19 @@ let print_token_list list =
 
 let token_list = Lexer.generate_token "CHOUQUETTE PARISBREST lol 3 pdpkdqopdq 45 () () () ) CHOUQUETTE PARISBREST CLAFOUTIS"
 
-let () = print_token_list (token_list)
-let () = print_newline ();;
+let () = print_string "Affichage de la liste de token après le tokenizer"; print_newline (); print_token_list (token_list); print_newline (); print_newline ()
 
 let () = print_string "Vérification du parenthésage et des quote"; print_newline ()
-let () = if Lexer.validate_parenthesis_and_quote token_list then print_string "yay" else failwith "parenthesage invalide"; print_newline ()
+let () = if Lexer.validate_parenthesis_and_quote token_list then print_string "Parenthesage valide" else failwith "parenthesage invalide"; print_newline (); print_newline ()
 let array = Array.of_list token_list
 
-let () = print_string "Vérification du parsing d'un string entre \""; print_newline ()
-let a,b = Parser.parse_string 1 array;;
-let () = print_string a; print_newline ();  print_int b
+let () = print_string "Vérification du parsing d'un string entre \" "; print_newline ()
+
+let rec i n lst = match (n,lst) with
+  | n,_::q when n<1 -> i (n+1) q 
+  | n,_::q when n=1 -> let a,_ = Parser.parse_string_rec q in a
+  | _ -> "";;
+
+let a = i 0 token_list;;
+let () = print_string ("String calculé: "^ a); print_newline ();
+
